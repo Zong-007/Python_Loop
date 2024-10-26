b2b8b6b16115f34bb81c68f70e655214b4cc47df
Zong-007 <nontasit2411@gmail.com>
2024-10-26 13:41:42 +0700

    Update EX6.py

---
+++ EX6.py
@@ -0,0 +1,20 @@
+"""
+ใช้งานคำสั่ง For Loop ให้แสดงผลตาม Output
+
+Input
+1, 2, 3, 4, 5
+
+Output
+Square of:  1 is:  1
+Square of:  2 is:  4
+Square of:  3 is:  9
+Square of:  4 is:  16
+Square of:  5 is:  25
+ 
+เติม code ให้สมบูรณ์ตามเงื่อนไข
+
+"""
+number = [1, 2, 3, 4, 5]
+
+for x in  :
+    print("Square of: ", , , x*)
