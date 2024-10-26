6a8ac6f619c9bc07828bad5d62d8e937fc89c4ca
Zong-007 <nontasit2411@gmail.com>
2024-10-26 13:37:29 +0700

    Update EX5.py

---
+++ EX5.py
@@ -0,0 +1,26 @@
+"""
+ใช้งานคำสั่ง For Loop ให้แสดงผลตาม Output
+
+Input
+"apple", "banana", "coconut"
+"red", "yellow", "green"
+
+Output
+apple  is  red
+banana  is  yellow
+coconut  is  green
+ 
+เติม code ให้สมบูรณ์ตามเงื่อนไข
+
+"""
+fruits = []
+color = []
+
+for x in fruits :
+    if  == "apple":
+        print(," is ",color[0])
+    if  == "banana":
+        print(," is ",color[])
+    if  == "coconut":
+        print(," is ",color[])
+
