3ee0a1240e20c0c64de364074ffa197f77c23067
Zong-007 <nontasit2411@gmail.com>
2024-10-26 14:07:07 +0700

    Update EX9.py

---
+++ EX9.py
@@ -0,0 +1,28 @@
+"""
+พิมพ์ตารางสูตรคูณของตัวเลข 5 หลักแรกโดยใช้ for loop และ while loop
+
+Input
+range(1, 6)
+
+Output
+Multiplication table of:  1
+1 2 3 4 5 6 7 8 9 10
+Multiplication table of:  2
+2 4 6 8 10 12 14 16 18 20
+Multiplication table of:  3
+3 6 9 12 15 18 21 24 27 30
+Multiplication table of:  4
+4 8 12 16 20 24 28 32 36 40
+Multiplication table of:  5
+5 10 15 20 25 30 35 40 45 50
+ 
+เติม code ให้สมบูรณ์ตามเงื่อนไข
+
+"""
+
+for x in range(_,_) :
+    print(" ")
+    print("Multiplication table of: ",)
+    for i in range(_,_):
+        print(*i,end=" ")
+
