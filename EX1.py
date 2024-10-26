6169144349b5fd07c50be8f34db5519fc57a346c
Zong-007 <nontasit2411@gmail.com>
2024-10-26 13:12:16 +0700

    Update EX1.py

---
+++ EX1.py
@@ -0,0 +1,19 @@
+"""
+ใช้งานคำสั่ง For Loop ให้แสดงผลตาม Output
+
+Input
+"geeks", "for", "geeks"
+
+Output
+geeks
+for
+geeks
+ 
+เติม code ให้สมบูรณ์ตามเงื่อนไข
+
+"""
+
+list = []
+
+for x in :
+  print()
