ce4c873cb0098770636ee04e64b6879424541ecf
Zong-007 <nontasit2411@gmail.com>
2024-10-26 13:45:03 +0700

    Update EX7.py

---
+++ EX7.py
@@ -0,0 +1,26 @@
+"""
+แสดงผลเลขคู่ และเลขคี่ ทั้งหมด ตามจำนวนที่กำหนดให้ ใน input
+
+Input
+1, 11
+
+Output
+Odd Number:  1
+Even Number:  2
+Odd Number:  3
+Even Number:  4
+Odd Number:  5
+Even Number:  6
+Odd Number:  7
+Even Number:  8
+Odd Number:  9
+Even Number:  10
+ 
+เติม code ให้สมบูรณ์ตามเงื่อนไข
+
+"""
+for x in range(1,11) :
+    if  ==  :
+        print("Even Number: ",)
+    else:
+        print("Odd Number: ",)
